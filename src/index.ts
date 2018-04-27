74920c8031a63e0cbe658bb4f0f6395200d9aba5
Nam Nguyen <nam.nguyen.node@gmail.com>
2018-04-27 16:35:20 +0700

    Try it!

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import * as koa from 'koa'
+import * as route from 'koa-router'
+
+const router = new route()
+
+router.get('/', ctx => ctx.body = "Hi there")
+
+const app = new koa()
+
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+app.listen(process.env.PORT || 3000)
--- typings/tsd.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference path="node/node.d.ts" />
-/// <reference path="express/express.d.ts" />
